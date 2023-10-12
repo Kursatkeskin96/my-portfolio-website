f592a7bed3d6ddf103ec7a4c920b2c118ff7c83a
Kursat Keskin <kursatkeskinn@gmail.com>
2023-10-12 23:37:03 +0100

    default theme edited

--- components/providers.jsx
+++ components/providers.jsx
@@ -3,5 +3,5 @@
 import { ThemeProvider } from 'next-themes'
 
 export default function Providers({ children }) {
-  return <ThemeProvider attribute='class'>{children}</ThemeProvider>
+  return <ThemeProvider defaultTheme='dark' attribute='class'>{children}</ThemeProvider>
 }
